2935b6b1a459bfb99a69544955371a181a365222
gitee-wangliang <1991wangliang@gmail.com>
2019-12-12 22:45:50 +0800

    Initial commit

---
+++ src/test/java/com/codingapi/tcp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingapi.tcp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
